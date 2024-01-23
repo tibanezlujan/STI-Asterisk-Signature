1df950fd5d86a5ad6a268dea85271e3a1e733279
tibanezlujan <tibanezlujan@gmail.com>
2024-01-23 14:30:20 +0100

    Added config file

--- README.md
+++ README.md
@@ -1,35 +1,50 @@
-
 *STI signature generator*
 
 In order to mitigate robocalls, STIR/SHAKEN has started to be implemented in different countries like United States or France. This suite of protocols is used for avoiding robocalls and callerId spoofing via PSTN. STIR (Secure Telephone Identity Revisited) SHAKEN (Signature-based Handling of Asserted information using toKENs), as explained in Wikipedia, is a uite of protocols and procedures intended to combat caller ID spoofing on public telephone networks. Caller ID spoofing is used by robocallers to mask their identity or to make it appear the call is from a legitimate source.
 
 Some old PBX/SBC are not ready for the usage of STIR/SHAKEN. Others can only use a limited number of certificates to sign calls. In order to start signing calls and send them to PSTN (or transit carrier), this script has been deployed and implemented. This can be easily implemented in old Asterisk boxes.
 
-The level of assertation of the call (A,B or C) will be decided taking into account if the orig_tn is defined in the DDI files named as xNumbersSTI.txt. This means that if for a call orig_tn is contained in the aNumbersSTI.txt, the call will be signed with attestation level "A". On the other hand, if that orig_tn is in bNumbersSTI.txt. Finally, if that orig_tn is contained in cNumbersSTI.txt or is not contained, then the call will be signed with "C".
+The level of assertation of the call (A, B or C) will be decided taking into account if the orig_tn is defined in the DDI files named as xNumbersSTI.txt. This means that if for a call orig_tn is contained in the aNumbersSTI.txt, the call will be signed with attestation level "A". On the other hand, if that orig_tn is in bNumbersSTI.txt. Finally, if that orig_tn is contained in cNumbersSTI.txt or is not contained, then the call will be signed with "C".
+
 
 USAGE:
 
 	bash> python3 STIsigner.py +cc +orig_tn +dest_tn pathToPrivateKey pathToPublicKey http://certificate-website.net
 
-	+cc 			= Country  Code where the call is placed
-	+orig_tn 		= Origin of call (Calling number)
-	+dest_tn		= Destination of call (Called number)
-	pathToPrivateKey	= Location where the private key is stored in the PBX server
-	pathToPublicKey		= Location where the public certificate/key is stored in the PBX server
+		+cc 			= Country  Code where the call is placed
+		+orig_tn 		= Origin of call (Calling number)
+		+dest_tn		= Destination of call (Called number)
+		pathToPrivateKey	= Location where the private key is stored in the PBX server
+		pathToPublicKey		= Location where the public certificate/key is stored in the PBX server
 
 
 FILES/FOLDER STRUCTURE:
 
-	app-folder/
+	script-folder/
 	|
 	|-PKI
 	|    |-priv_serverPKI.key (Default Private Key)
-	|    |-pub_serverPKI.key  (Default Public Key)
+	|    |-pub_serverPKI.cert  (Default Public Key)
 	|
 	|-DDI
 	     |-aNumbersSTI.txt (List of A numbers)
 	     |-bNumbersSTI.txt (List of B numbers)
 	     |-cNumbersSTI.txt (List of C numbers)
+
+
+MODULES:
+
+	- json
+	- jwt
+	- calendar
+	- time
+	- sys
+
+
+RELATED PROJECTS:
+
+	https://github.com/jpadilla/pyjwt/
+
 
 LIMITATIONS:
 
--- STIsigner.py
+++ STIsigner.py
@@ -2,15 +2,6 @@ import json,jwt
 import calendar,time
 import sys
 
-
-################################################################################
-# DOCUMENTATION
-################################################################################
-#
-# https://github.com/jpadilla/pyjwt/ 
-#
-
-
 ################################################################################
 #FUNCTIONS
 ################################################################################
@@ -31,15 +22,11 @@ def checkOriginAttestB(orig_tn):
             return(True)
     return(False)
 
-
 def attestation_level_call(orig_tn,dest_tn,cc):
         lenCC=len(cc)
         prefixA=orig_tn[1:lenCC]
         prefixB=dest_tn[1:lenCC]
         prefixCC=cc[1:]
-
-        #Check if is national call
-        #print(prefixA+" "+prefixB+" "+prefixCC)
         if prefixA==prefixB and prefixA==prefixCC:
 
                 if checkOriginAttestA(orig_tn):
@@ -48,11 +35,10 @@ def attestation_level_call(orig_tn,dest_tn,cc):
                     return("B")
                 else:
                     return("C")
-
         else:
                 return("C")
 
-def generate_payload_STI_json_Text(attest,dest_tn,orig_tn,origid):
+def generate_payload_STI_json_Text(attest,dest_tn,orig_tn,ORIGID):
     dest_tn=dest_tn[1:]
     orig_tn=orig_tn[1:]
     epochSecs=calendar.timegm(time.gmtime())
@@ -63,11 +49,11 @@ def generate_payload_STI_json_Text(attest,dest_tn,orig_tn,origid):
         "dest":{"tn":dest_tn},
         "iat":str(iat),
         "orig":{"tn":orig_tn},
-        "origid":origid,
+        "ORIGID":ORIGID,
     }
     return(json.dumps(payloadSTI, indent=4))
 
-def generate_payload_STI_json_Object(attest,dest_tn,orig_tn,origid):
+def generate_payload_STI_json_Object(attest,dest_tn,orig_tn,ORIGID):
     dest_tn=dest_tn[1:]
     orig_tn=orig_tn[1:]
     epochSecs=calendar.timegm(time.gmtime())
@@ -78,11 +64,11 @@ def generate_payload_STI_json_Object(attest,dest_tn,orig_tn,origid):
         "dest":{"tn":dest_tn},
         "iat":iat,
         "orig":{"tn":orig_tn},
-        "origid":origid,
+        "ORIGID":ORIGID,
     }
     return(payloadSTI)
 
-def generate_header_STI_json_Object(algo,urlCert):
+def generate_header_STI_json_Object(alg,urlCert):
     headerSTI = {
         "ppt":"shaken",
         "typ":"passport",
@@ -108,6 +94,47 @@ def load_public_key(public_key_file_pem):
         public_key = public_key_file.read()
         return(public_key)
 
+def load_config():
+    file="STIsigner.cfg"
+    f=open(file, "r")
+    for line in f:
+        line=line.rstrip('\n')
+        if "ALGOR" in line:
+            global global_ALGOR
+            global_ALGOR=line.split('=')
+            global_ALGOR=global_ALGOR[1]
+            #print(">>"+global_ALGOR)
+        elif "BYPASS" in line:
+            global global_BYPASS
+            global_BYPASS=line.split('=')
+            global_BYPASS=global_BYPASS[1]
+            #print(">>"+str(global_BYPASS))
+        elif "ORIGID" in line:
+            global global_ORIGID
+            global_ORIGID=line.split('=')
+            global_ORIGID=global_ORIGID[1]
+            #print(">>"+global_ORIGID)
+        elif "PRI_KEY" in line:
+            global global_PRI_KEY
+            global_PRI_KEY=line.split('=')
+            global_PRI_KEY=global_PRI_KEY[1]
+            #print(">>"+global_PRI_KEY)
+        elif "PUB_CERT" in line:
+            global global_PUB_CERT
+            global_PUB_CERT=line.split('=')
+            global_PUB_CERT=global_PUB_CERT[1]
+            #print(">>"+global_PUB_CERT)
+        elif "URL_CERT" in line:
+            global global_URL_CERT
+            global_URL_CERT=line.split('=')
+            global_URL_CERT=global_URL_CERT[1]
+            #print(">>"+global_URL_CERT)
+        else:
+            return(False)
+
+    return(True)
+
+
 ################################################################################
 # MAIN BODY
 ################################################################################
@@ -115,17 +142,34 @@ def load_public_key(public_key_file_pem):
 #public_key_file_pem="PKI/pub_serverPKY.cert"
 #url_cert="http://voice-test-tool-colt.net/sti-repo/pizzatelecom.crt"
 
-#STEP 1: Define the Alg
-#algo="ES256"
-algo="RS256"
-BY_PASS="OPE00-999999-99999-9999"
+#STEP 1: Intialize global variables
+global global_ALGOR 
+global_ALGOR=""
+global global_BYPASS
+global_BYPASS = ""
+global global_ORIGID
+global_ORIGID = ""
+global global_PRI_KEY
+global_PRI_KEY = ""
+global global_PUB_CERT
+global_PUB_CERT = ""
+global global_URL_CERT
+global_URL_CERT = ""
 
-#STEP 2: Take generate origid
-origid="123e4567-e89b-12d3-a456-426655440000"
+
+#STEP 2: Load configuration
+config=load_config()
+
+if config == False:
+    print("Error: The configuration file has not been loaded")
+    exit()
+    
 
 
 #STEP 3: Take arguments from CLI. If there are no arguments ask for them interactively:
 try:
+
+    if len(sys.argv) > 5:
         cc=sys.argv[1]
         orig_tn=sys.argv[2]
         dest_tn=sys.argv[3]
@@ -133,11 +177,23 @@ try:
         public_key_file_pem=sys.argv[5]
         url_cert=sys.argv[6]
         #print(sys.argv)
+    elif len(sys.argv) < 5:
+        cc=sys.argv[1]
+        orig_tn=sys.argv[2]
+        dest_tn=sys.argv[3]
+        private_key_file_pem=global_PRI_KEY
+        public_key_file_pem=global_PUB_CERT
+        url_cert=global_URL_CERT
 
+    else:
         if cc=="--help":
-            print("Usage:")
+            print("HOW TO USE:")
             print("python3 STIsigner_v2.py +cc +orig_tn +dest_tn locationPrivateKeu LocationPublicKey urlCertificate")
 
+        else:
+            print("Error: Incorrect number of arguments")
+            print("HOW TO USE:")
+            print("python3 STIsigner_v2.py +cc +orig_tn +dest_tn locationPrivateKeu LocationPublicKey urlCertificate")
 
 except Exception as err:
         if len(sys.argv) < 2 or cc=="--help":
@@ -152,7 +208,7 @@ except Exception as err:
             exit()
         else:
             #print("WARNING: Unexpected {err=}, {type(err)=}\n")
-            print("P-Bypass-Identity: "+BY_PASS)
+            print("P-Bypass-Identity: "+global_BYPASS)
             exit()
 
 if cc!="" and dest_tn!="" and orig_tn!="" and private_key_file_pem!="" and  public_key_file_pem!="" and url_cert!="":
@@ -165,11 +221,11 @@ if cc!="" and dest_tn!="" and orig_tn!="" and private_key_file_pem!="" and  publ
     attest = attestation_level_call(orig_tn,dest_tn,cc)
 
     #STEP 7: Generate JSON to sign
-    payloadSTItxt = generate_payload_STI_json_Text(attest,dest_tn,orig_tn,origid)
-    payloadSTIob = generate_payload_STI_json_Object(attest,dest_tn,orig_tn,origid)
-    headerSTI=generate_header_STI_json_Object(algo,url_cert)
+    payloadSTItxt = generate_payload_STI_json_Text(attest,dest_tn,orig_tn,global_ORIGID)
+    payloadSTIob = generate_payload_STI_json_Object(attest,dest_tn,orig_tn,global_ORIGID)
+    headerSTI=generate_header_STI_json_Object(global_ALGOR,url_cert)
 
     #STEP 8: Encode the JSON
-    #sti_token = encode_Identity_noheader(payloadSTIob,private_key,algo)
-    sti_token = encode_Identity_header(payloadSTIob,private_key,algo,headerSTI)
-    print("Identity: "+sti_token+";info=<"+url_cert+">;alg="+algo+";ppt=shaken")
+    #sti_token = encode_Identity_noheader(payloadSTIob,private_key,global_ALGOR)
+    sti_token = encode_Identity_header(payloadSTIob,private_key,global_ALGOR,headerSTI)
+    print("Identity: "+sti_token+";info=<"+url_cert+">;alg="+global_ALGOR+";ppt=shaken")
